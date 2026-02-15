a837ca65d7409004f3e192bfaf7532489c1c7911
noob <papakisprits@gmail.com>
2026-02-15 13:00:38 +0200

    make the logic still work if the practice attempt is after a normal attempt

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,27 @@ using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
     struct Fields {
-        int m_sessionStartAttempt = 0;
+        int m_sessionAttemptCount = 0;
     };
 
     bool init(GJGameLevel* level, bool useReplay, bool dontSave) {
         if (!PlayLayer::init(level, useReplay, dontSave)) return false;
-        
-        m_fields->m_sessionStartAttempt = level->m_attempts;
+        m_fields->m_sessionAttemptCount = 0;
         return true;
     }
 
+    void resetLevel() {
+        PlayLayer::resetLevel();
+        m_fields->m_sessionAttemptCount++;
+    }
+
     void levelComplete() {
-        if (this->m_isPracticeMode && this->m_player1 && !this->m_player1->m_isDead) {
-            if (this->m_level->m_attempts - m_fields->m_sessionStartAttempt <= 1) {
-                this->m_isPracticeMode = false;
-                Notification::create("Practice run counted as normal", NotificationIcon::Success)->show();
-            }
+        if (this->m_isPracticeMode && 
+            m_fields->m_sessionAttemptCount <= 1 && 
+            this->m_player1 && !this->m_player1->m_isDead) {
+            
+            this->m_isPracticeMode = false;
+            Notification::create("Practice run counted as normal", NotificationIcon::Success)->show();
         }
 
         PlayLayer::levelComplete();
